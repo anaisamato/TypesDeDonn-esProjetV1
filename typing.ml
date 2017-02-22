ce1a8730e93476b96a2dab28002b836bf0bac75b
Anaïs <anaisamato81@gmail.com>
2017-02-22 12:08:50 +0100

    Exercice 2 -> Rajout de la fonctionnalité CallE et de commentaires dans le README

--- typing.ml
+++ typing.ml
@@ -18,7 +18,7 @@ let tp_prog (Prog (gvds, fdfs)) =
        [Fundefn (Fundecl (BoolT, "even", [Vardecl (IntT, "n")]), [], Skip)])
 ;;
 
-(* Exercice 1 -> TP_EXPR *)
+(* Exercices 1,2 -> TP_EXPR *)
 (* retrieve the type of vname in the environment*)
 let retrieve_type x = function env ->
     let rec retrieve_type_aux x = function 
@@ -26,6 +26,15 @@ let retrieve_type x = function env ->
      	|(a, b)::q -> if x=a then b else retrieve_type_aux x q
     in let localvar = env.localvar in retrieve_type_aux x localvar ;;	
 (* val retrieve_type : vname -> environment -> tp = <fun> *)
+
+(* check if a function is in the environment *)
+let rec check_function name = function env ->
+	let rec check_function_aux name = function
+		[] -> failwith"error, function unknown"
+		|Fundecl(t, n, []) ::l-> if name=n then Fundecl(t,n,[]) else check_function_aux name l
+		|Fundecl(t, n, Vardecl(a,b)::q)::l-> if name=n then Fundecl(t,n, Vardecl(a,b)::q) else check_function_aux name l
+	in let func= env.funbind in check_function_aux name func;;
+(*  val check_function : fname -> environment -> fundecl = <fun>*)
 
 (* transform a value in a type *)
 let verifConst = function
@@ -65,11 +74,34 @@ let rec retrieve_binop env = function
 								if val1 = BoolT && val2 = BoolT then BoolT else failwith"error";;
 (* val retrieve_binop : environment -> binop * 'a expr * 'b expr -> tp = <fun> *)
 
+(* check the arguments of the CallE and the Vardecl of the Fundecl *)
+let rec check_function_type = function
+	Fundecl(t,_,[]), [] -> t
+	|Fundecl(_,_,_), [] -> failwith"error"
+	|Fundecl(_,_,[]), _ -> failwith"error"
+	|(Fundecl(t,n,Vardecl(a,b)::q)), (Vardecl(c,d)::l) -> if a=c then check_function_type(Fundecl(t,n, q), l) else failwith"error";;
+(* val check_function_type : fundecl * vardecl list -> tp = <fun> *)
+
+
+(* transform a constant in a Varcecl *)
+let rec transform_del_vardecl env = function
+	[] -> []
+	|(Const(a,IntV b))::q -> Vardecl(IntT, string_of_int b)::transform_del_vardecl env q
+	|(Const(a,BoolV b))::q -> Vardecl(BoolT, string_of_bool b)::transform_del_vardecl env q;;
+(* val transform_del_vardecl : 'a -> 'b expr list -> vardecl list = <fun> *)
+
+(* check the type of Const in a list *)
+let rec check_function_type_list env = function
+	[] -> []
+	|Const(a,b)::q -> Const(verifConst(b), b)::check_function_type_list env q;;
+(* val check_function_type_list : 'a -> 'b expr list -> tp expr list = <fun> *)
 
 (* retrieve the type of expressions *)
 let rec tp_expr env = function
 	Const(0, v) -> Const(verifConst v, v)
 	|VarE (0,v)-> VarE((retrieve_type(retrieve_var v) env), v)
 	|BinOp (0, binop, exp1, exp2)-> BinOp(retrieve_binop env (binop, exp1,exp2), binop, tp_expr env exp1, tp_expr env exp2)
-	|IfThenElse(0, vIf, vThen, vElse) -> IfThenElse(retrieve_val env(IfThenElse(0,vIf, vThen, vElse)), tp_expr env vIf, tp_expr env vThen, tp_expr env vElse);;
+	|IfThenElse(0, vIf, vThen, vElse) -> IfThenElse(retrieve_val env(IfThenElse(0,vIf, vThen, vElse)), tp_expr env vIf, tp_expr env vThen, tp_expr env vElse)
+	|CallE(0, name, Const(a,b)::q) -> let func = check_function name env in let varD= transform_del_vardecl env (Const(a,b)::q) in 
+										CallE((check_function_type(func, varD)), name, check_function_type_list env (Const(a,b)::q));;
 (* val tp_expr : environment -> int expr -> tp expr = <fun> *)
