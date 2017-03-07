7f69c786c77fab559b8557f1c35d45e3ff2792a3
Anaïs <anaisamato81@gmail.com>
2017-03-07 09:33:30 +0100

    Fin exercice 4

--- gen.ml
+++ gen.ml
@@ -3,7 +3,6 @@
 open Lang;;
 open Analyses;;
 open Instrs;;
-open Print_instr;;
 (* ************************************************************ *)
 (* **** Compilation of expressions / statements            **** *)
 (* ************************************************************ *)
@@ -20,21 +19,22 @@ let gen_prog (Prog (gvds, fdfs)) =
                       Methinfo (3, 1),
                       [Loadc (IntT, IntV 0); ReturnI IntT])])
 
+
 (* find the position of x in a list *)
 let rec position x = function
   	[] -> failwith"Not in the list"
   	|a::q -> if a=x then 0 else 1+position x q;;
 (*  val position : 'a -> 'a list -> int = <fun> *)
 
+
 (* take a Var and return it value *)
 let varAux = function
   Var(_, a) -> a;;
-(* *)
-
+(* val varAux  var -> vname = <fun> *)
+ 
 (* generate bytecode *)
-let gen_exp liste = function
+let rec gen_exp liste = function
    Const(t, v)-> [Loadc (t ,v)]
-  |VarE (t,v)  -> [Loadv (t, position(varAux v) liste)];;
+  |VarE (t,v)  -> [Loadv (t, position(varAux v) liste)]
+  |BinOp (t, binop, exp1, exp2)-> (gen_exp liste exp1)@(gen_exp liste exp2)@([Bininst(t, binop)]);;
 (* val gen_exp : vname list -> tp expr -> instr list = <fun> *)
-
-
